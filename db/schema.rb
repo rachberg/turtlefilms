b6cd93ae1e6de97324f932259165624d707b9eba
Rachel Bergmann <rachelbergmann@gmail.com>
2015-02-12 18:10:14 +0100

    added fields to movie model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212163527) do
+ActiveRecord::Schema.define(version: 20150212164651) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
     t.integer  "year"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "language"
+    t.string   "genre"
+    t.string   "summary"
+    t.integer  "rt_rating"
+    t.integer  "imdb_rating"
+    t.string   "image_uri"
   end
 
 end
