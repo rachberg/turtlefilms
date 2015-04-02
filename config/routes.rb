c1216945cda9378c3c0448beba5ea75864fbed4a
Rachel Bergmann <rachelbergmann@gmail.com>
2015-04-02 21:15:29 +0200

    spruced up nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-root 'application#hello'
+root 'movies#index'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
